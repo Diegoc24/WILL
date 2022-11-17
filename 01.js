82007a4ee4966f9a14d4488077c59acac691a58f
Diegoc24 <carusodiegod@gmail.com>
2022-11-17 19:29:48 -0300

    prim

--- 01.js
+++ 01.js
@@ -11,7 +11,17 @@ function soloNumeros(array) {
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
   // Tu código aca:
+  var numero = [];
+  for(i = 0;i < array.length;i++){
 
+    if(array[i] === Number){
+
+      numero.push(array[i]);
+
+    }
+
+  }
+  return numero;
 }
 
 // No modifiques nada debajo de esta linea //

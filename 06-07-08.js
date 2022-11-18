ae322696a4caa4e4da079214bacf1e2378999524
Diegoc24 <carusodiegod@gmail.com>
2022-11-18 14:18:29 -0300

    6 fin

--- 06-07-08.js
+++ 06-07-08.js
@@ -11,6 +11,12 @@ function crearClasePersona() {
       // Inicializar las propiedades de la persona con los valores recibidos como argumento
 
       // Tu código aca:
+      this.nombre = nombre;
+      this.edad = edad;
+      this.hobbies = hobbies;
+      this.amigos = amigos;
+
+
 
     }
 
@@ -20,7 +26,7 @@ function crearClasePersona() {
       // No debe retornar nada.
 
       // Tu código aca:
-
+      
     }
 
     addHobby(hobby) {
